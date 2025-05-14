28b6a6f2194d26722ac369d39b467e0111a64f0d
Rayhan Khan <rayhankhan1226@gmail.com>
2025-05-14 21:11:52 +0600

    added new detection features from real time webcam

--- server/myapp/urls.py
+++ server/myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import PredictView
-from .genai import Detect
+from .genai import Detect, VideoStreamDetectionView, VideoStreamMJPEGView
 
 urlpatterns = [
     path('predict/', PredictView.as_view(), name='predict'),
-    path('detect_damage/', Detect.as_view(), name='detect_damage')
+    path('detect_damage/', Detect.as_view(), name='detect_damage'),
+    #path("detect/", LiveYOLODetectionView.as_view(), name="live-detection"),
+    path("detect/", VideoStreamDetectionView.as_view(), name="detect"),
+    path('live-stream/', VideoStreamMJPEGView.as_view(), name='live_stream'),
 ]
